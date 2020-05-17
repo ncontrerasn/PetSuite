87614a1cc7a28b8e6f3a8c7fcd9804108a72c14e
htovars <htovars@unal.edu.co>
2020-05-17 16:15:21 -0500

    search implementation for dogdaycare

--- src/main/java/com/petsuite/Services/repository/DogDaycareRepository.java
+++ src/main/java/com/petsuite/Services/repository/DogDaycareRepository.java
@@ -1,10 +1,13 @@
 package com.petsuite.Services.repository;
 
+import com.petsuite.Services.model.Dog;
 import com.petsuite.Services.model.DogDaycare;
+import com.petsuite.Services.model.InfoUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 public interface DogDaycareRepository extends JpaRepository<DogDaycare, String>{
 
@@ -22,5 +25,8 @@ public interface DogDaycareRepository extends JpaRepository<DogDaycare, String>{
     @Modifying
     @Query(value = "UPDATE dog_daycare SET dog_daycare_type = ?1 WHERE user = ?2", nativeQuery = true)
     Integer updateType(boolean type, String user);
+
+    @Query(value = "SELECT user FROM info_user where name like ?1 AND role = 'ROLE_DOGDAYCARE'", nativeQuery = true)
+    List<String> searchByName(String name);
 
 }
--- src/main/java/com/petsuite/controller/DogDayCareController.java
+++ src/main/java/com/petsuite/controller/DogDayCareController.java
@@ -9,8 +9,10 @@ import com.petsuite.Services.dto.Client_Dto;
 import com.petsuite.Services.dto.DogDayCare_Dto;
 import com.petsuite.Services.model.DogDaycare;
 import com.petsuite.Services.model.DogWalker;
+import com.petsuite.Services.model.InfoUser;
 import com.petsuite.Services.repository.DogDaycareRepository;
 import com.petsuite.Services.repository.InfoUserRepository;
+import com.petsuite.basics.Cadena;
 import com.petsuite.basics.CadenaDoble;
 import com.petsuite.basics.Entero;
 import io.jsonwebtoken.Jwts;
@@ -220,6 +222,58 @@ public class DogDayCareController {
 
         return DayCare_DTO;
 
+    }
+
+    @PostMapping("/searchbyname")
+    public List<DogDayCare_Dto> updateAll(@Valid @RequestBody Cadena name){
+
+        System.out.println(name);
+
+         List<String> usersToReturns = new ArrayList<>();
+
+        List<String> findings = new ArrayList<>();
+
+        String[] Words = name.getCadena().split(" ");
+
+        for (int i=0; i<Words.length; i++){
+            System.out.println(Words);
+            findings = dogDaycareRepository.searchByName("%"+Words[i]+"%");
+
+            while(!findings.isEmpty()){
+                if (!usersToReturns.contains(findings.get(0))) {
+                    usersToReturns.add(findings.get(0));
+                }
+                findings.remove(0);
+            }
+        }
+
+        List<DogDayCare_Dto> returns = new ArrayList<>();
+
+        DogDayCare_Dto DTO;
+
+        Optional<DogDaycare> DC;
+
+        while(!usersToReturns.isEmpty()){
+
+            DC = dogDaycareRepository.findById(usersToReturns.remove(0));
+
+            System.out.println(DC.get().getUser());
+
+            DTO = new DogDayCare_Dto();
+
+            DTO.setDog_daycare_name(DC.get().getName());
+            DTO.setDog_daycare_type(DC.get().getDog_daycare_type());
+            DTO.setDog_daycare_score(DC.get().getDog_daycare_score());
+            DTO.setDog_daycare_phone(DC.get().getPhone());
+            DTO.setDog_daycare_address(DC.get().getDog_daycare_address());
+            DTO.setUser(DC.get().getUser());
+            DTO.setDog_daycare_e_mail(DC.get().getE_mail());
+
+            returns.add(DTO);
+
+        }
+
+        return returns;
     }
 
     private String getJWTToken(String username) {
