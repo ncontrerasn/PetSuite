3bf5a8aa01b9331c9dd82a19f927f0d49db7c5e6
ncontrerasn <ncontrerasn@unal.edu.co>
2020-05-17 16:34:08 -0500

    Closes #35

--- src/main/java/com/petsuite/controller/DogDayCareInvoiceController.java
+++ src/main/java/com/petsuite/controller/DogDayCareInvoiceController.java
@@ -1,20 +1,15 @@
 package com.petsuite.controller;
 
 import com.petsuite.Services.dto.DogDayCareInvoice_Dto;
-import com.petsuite.Services.dto.DogDayCare_Dto;
+import com.petsuite.Services.model.DogDayCareService_DogDayCareInvoice;
 import com.petsuite.Services.model.DogDaycare;
 import com.petsuite.Services.model.DogDaycareInvoice;
-import com.petsuite.Services.repository.DogDaycareInvoiceRepository;
-import com.petsuite.Services.repository.DogDaycareRepository;
-import com.petsuite.Services.repository.InfoUserRepository;
-import com.petsuite.basics.EnteroDoble;
+import com.petsuite.Services.repository.*;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.*;
@@ -33,43 +28,56 @@ public class DogDayCareInvoiceController {
 
     @Autowired
     DogDaycareInvoiceRepository dogDaycareInvoiceRepository;
-    
+
     @Autowired
-    private JdbcTemplate jdbcTemplate;
-    
-    Service_invoiceController service_invoiceController= new Service_invoiceController();
-    
-    
+    Service_InvoiceRepository service_InvoiceRepository;
+
+    @Autowired
+    DogDaycareServiceRepository dogDaycareServiceRepository;
+
+    @Autowired
+    DogRepository dogRepository;
   
-     @GetMapping("/all")
+    @GetMapping("/all")
     public List<DogDaycareInvoice> getAllClients() {
       return dogDaycareInvoiceRepository.findAll();
     }
 
     @PostMapping("/load")//Retorna una estructura de tipo DogDaycare vacia si ya esta utilizado el nombre de usuario
     public DogDayCareInvoice_Dto createDogDaycareInvoice(@Valid @RequestBody DogDayCareInvoice_Dto dogDaycareInovice) {
-        //para formatear la fecha
-        System.out.println("Estamos creando un recibo de guarderia");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            LocalDateTime dateTime = LocalDateTime.parse(dogDaycareInovice.getDog_daycare_invoice_date(), formatter);
-            Float price= dogDaycareInovice.getDog_daycare_invoice_duration()*(dogDaycareRepository.findById(dogDaycareInovice.getDog_daycare_invoice_dogdaycare_id()).get().getDog_daycare_base_price());
-        System.out.println("El dateTimeingresado es : "+ dateTime);
-            DogDaycareInvoice daycareInvoice= new DogDaycareInvoice(null, dateTime, dogDaycareInovice.getDog_daycare_invoice_duration(), price, dogDaycareInovice.getDog_daycare_invoice_status(), dogDaycareInovice.getDog_daycare_invoice_dogdaycare_id(), dogDaycareInovice.getDog_daycare_invoice_client_id(), dogDaycareInovice.getDog_daycare_invoice_dog_id(), null, null, null, null);
-        
+        LocalDateTime dateTime = LocalDateTime.parse(dogDaycareInovice.getDog_daycare_invoice_date(), formatter);
+        float dogWeight = dogRepository.findByDogId(dogDaycareInovice.getDog_daycare_invoice_dog_id()).getDog_weight();
+        DogDaycare dogDaycare = dogDaycareRepository.findById(dogDaycareInovice.getDog_daycare_invoice_dogdaycare_id()).get();
+        //precio base por la estadía
+        Float price = dogDaycareInovice.getDog_daycare_invoice_duration() * dogDaycare.getDog_daycare_base_price();
+        //perro pesado es mayor o igual que 28 kg
+        if(dogWeight >= 28)
+            price += dogDaycareInovice.getDog_daycare_invoice_duration() * dogDaycare.getDog_daycare_tax();
+        //más el precio por los servicios
+        for (int i = 0; i < dogDaycareInovice.getDog_daycare_invoice_services().size(); i++)
+            price += dogDaycareServiceRepository.findById(
+                    dogDaycareInovice.getDog_daycare_invoice_services().get(i)).get().getDogdaycare_Service_Price();
+
+        DogDaycareInvoice daycareInvoice= new DogDaycareInvoice(null, dateTime,
+                dogDaycareInovice.getDog_daycare_invoice_duration(), price, dogDaycareInovice.getDog_daycare_invoice_status(),
+                dogDaycareInovice.getDog_daycare_invoice_dogdaycare_id(), dogDaycareInovice.getDog_daycare_invoice_client_id(),
+                dogDaycareInovice.getDog_daycare_invoice_dog_id(), null, null, null, null);
+        //guardar el recibo
         if(daycareInvoice!=null){
             dogDaycareInovice.setDog_daycare_invoice_price(price);
-            System.out.println(dogDaycareInovice.getDog_daycare_invoice_services());
-            dogDaycareInvoiceRepository.save(daycareInvoice);
-            service_invoiceController.setJdbcTemplate(jdbcTemplate);
-            for (int i = 0; i < dogDaycareInovice.getDog_daycare_invoice_services().size() ; i++) {
-                service_invoiceController.createDogDaycareServiceInvoice(new EnteroDoble(daycareInvoice.getDog_daycare_invoice_id(), dogDaycareInovice.getDog_daycare_invoice_services().get(i)),daycareInvoice);
+            DogDaycareInvoice dogDaycareInvoice = dogDaycareInvoiceRepository.saveAndFlush(daycareInvoice);
+            //registro en la tabla intermedia
+            for (int i = 0; i < dogDaycareInovice.getDog_daycare_invoice_services().size(); i++) {
+                DogDayCareService_DogDayCareInvoice dogDayCareService_dogDayCareInvoice = new DogDayCareService_DogDayCareInvoice();
+                dogDayCareService_dogDayCareInvoice.setDogDaycareInvoice(dogDaycareInvoice);
+                dogDayCareService_dogDayCareInvoice.setDogDaycareService(dogDaycareServiceRepository.findById(
+                        dogDaycareInovice.getDog_daycare_invoice_services().get(i)).get());
+                service_InvoiceRepository.save(dogDayCareService_dogDayCareInvoice);
             }
-            
-            
             return dogDaycareInovice;
         }
         return null;
-      
     }
 
     private String getJWTToken(String username) {
--- src/main/java/com/petsuite/controller/Service_invoiceController.java
+++ src/main/java/com/petsuite/controller/Service_invoiceController.java
@@ -1,101 +1,10 @@
 package com.petsuite.controller;
 
-import com.petsuite.Services.compositeKey.Service_Invoice_Id;
-import com.petsuite.Services.dto.DogDayCareInvoice_Dto;
-import com.petsuite.Services.dto.DogDayCare_Dto;
-import com.petsuite.Services.model.DogDayCareService_DogDayCareInvoice;
-import com.petsuite.Services.model.DogDaycare;
-import com.petsuite.Services.model.DogDaycareInvoice;
-import com.petsuite.Services.repository.DogDaycareInvoiceRepository;
-import com.petsuite.Services.repository.DogDaycareRepository;
-import com.petsuite.Services.repository.DogDaycareServiceRepository;
-import com.petsuite.Services.repository.InfoUserRepository;
-import com.petsuite.Services.repository.Service_InvoiceRepository;
-import com.petsuite.basics.EnteroDoble;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/service_invoices")
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class Service_invoiceController {
-
-    @Autowired
-    DogDaycareRepository dogDaycareRepository;
-
-    @Autowired
-    DogDaycareInvoiceRepository dogDaycareInvoiceRepository;
-    @Autowired
-    DogDaycareServiceRepository dogDaycareServiceRepository;
-    
-    
-    @Autowired
-    Service_InvoiceRepository service_InvoiceRepository ;
-    
-    
-    
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    
-    @Autowired
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-
-     @GetMapping("/all")
-    public List<DogDayCareService_DogDayCareInvoice> getAllClients() {
-      return service_InvoiceRepository.findAll();
-    }
-
-   
-    public DogDayCareService_DogDayCareInvoice createDogDaycareServiceInvoice( EnteroDoble enteroDoble, DogDaycareInvoice dogDaycareInvoice) {
-       DogDayCareService_DogDayCareInvoice careInvoice= new DogDayCareService_DogDayCareInvoice();
-       
-        System.out.println("El entero doble que me llega es: "+ enteroDoble);
-       careInvoice.setDogDaycareInvoice(dogDaycareInvoice);
-        System.out.println(dogDaycareServiceRepository.findById(1));
-       careInvoice.setId(new Service_Invoice_Id(enteroDoble.getEntero1(),enteroDoble.getEntero2()));
-       
-       if(careInvoice.getDogDaycareInvoice()!=null && careInvoice.getDogDaycareService()!=null){
-           
-           service_InvoiceRepository.save(careInvoice);
-           return careInvoice;
-       }
-      return null;
-    }
-
-    private String getJWTToken(String username) {
-        String secretKey = "mySecretKey";
-        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
-                .commaSeparatedStringToAuthorityList("DOGDAYCARE");
-
-        String token = Jwts
-                .builder()
-                .setId("softtekJWT")
-                .setSubject(username)
-                .claim("authorities",
-                        grantedAuthorities.stream()
-                                .map(GrantedAuthority::getAuthority)
-                                .collect(Collectors.toList()))
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 600000))
-                .signWith(SignatureAlgorithm.HS512,
-                        secretKey.getBytes()).compact();
-
-        return "Bearer " + token;
-    }
 
 }
