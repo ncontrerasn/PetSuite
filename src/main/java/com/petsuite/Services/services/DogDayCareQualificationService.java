b65fca4d6bd548e374a806a592a9bde218125b4e
ncontrerasn <ncontrerasn@unal.edu.co>
2020-05-21 22:52:59 -0500

    Closes #39

---
+++ src/main/java/com/petsuite/Services/services/DogDayCareQualificationService.java
@@ -0,0 +1,32 @@
+package com.petsuite.Services.services;
+
+import com.petsuite.Services.dto.DogDayCareInvoice_Dto;
+import com.petsuite.Services.repository.DogDaycareInvoiceRepository;
+import com.petsuite.Services.repository.DogDaycareRepository;
+import com.petsuite.basics.Cadena;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DogDayCareQualificationService implements DogDayCareQualification {
+
+    @Autowired
+    DogDaycareInvoiceRepository dogDaycareInvoiceRepository;
+
+    @Autowired
+    DogDaycareRepository dogDaycareRepository;
+
+    @Override
+    public Cadena qualifyDogDayCare(DogDayCareInvoice_Dto dogDayCareInvoice_dto) {
+        int updatedInvoice = dogDaycareInvoiceRepository.scoreDogDaycare(dogDayCareInvoice_dto.getDog_daycare_invoice_score(),
+                dogDayCareInvoice_dto.getDog_daycare_invoice_dog_id());
+        if(updatedInvoice == 1){
+            float score = dogDaycareInvoiceRepository.scoreAvg(dogDayCareInvoice_dto.getDog_daycare_invoice_dogdaycare_id());
+            int updatedDogDaycareScore = dogDaycareRepository.updateScore(score,
+                    dogDayCareInvoice_dto.getDog_daycare_invoice_dogdaycare_id());
+            if(updatedDogDaycareScore == 1)
+                return new Cadena("Guardería calificada correctamente");
+        }
+        return new Cadena("Error calificando la guardería");
+    }
+}
--- src/main/java/com/petsuite/controller/DogDayCareInvoiceController.java
+++ src/main/java/com/petsuite/controller/DogDayCareInvoiceController.java
@@ -5,6 +5,7 @@ import com.petsuite.Services.model.DogDayCareService_DogDayCareInvoice;
 import com.petsuite.Services.model.DogDaycare;
 import com.petsuite.Services.model.DogDaycareInvoice;
 import com.petsuite.Services.repository.*;
+import com.petsuite.Services.services.DogDayCareQualificationService;
 import com.petsuite.Services.services.EndCareService;
 import com.petsuite.basics.Cadena;
 import com.petsuite.basics.Entero;
@@ -40,6 +41,9 @@ public class DogDayCareInvoiceController {
 
     @Autowired
     DogRepository dogRepository;
+
+    @Autowired
+    DogDayCareQualificationService dogDayCareQualificationService;
     
     //Importamos el servicio a ustilizar de erminar el servicio de cuidado
     @Autowired
@@ -105,16 +109,7 @@ public class DogDayCareInvoiceController {
 
     @PostMapping("/score")
     public Cadena scoreDogDayCare(@Valid @RequestBody DogDayCareInvoice_Dto dogDayCareInvoice_dto){
-        int updatedInvoice = dogDaycareInvoiceRepository.scoreDogDaycare(dogDayCareInvoice_dto.getDog_daycare_invoice_score(),
-                dogDayCareInvoice_dto.getDog_daycare_invoice_dog_id());
-        if(updatedInvoice == 1){
-            float score = dogDaycareInvoiceRepository.scoreAvg(dogDayCareInvoice_dto.getDog_daycare_invoice_dogdaycare_id());
-            int updatedDogDaycareScore = dogDaycareRepository.updateScore(score,
-                    dogDayCareInvoice_dto.getDog_daycare_invoice_dogdaycare_id());
-            if(updatedDogDaycareScore == 1)
-                return new Cadena("Guardería calificada correctamente");
-        }
-        return new Cadena("Error calificando la guardería");
+        return dogDayCareQualificationService.qualifyDogDayCare(dogDayCareInvoice_dto);
     }
 
     private String getJWTToken(String username) {
